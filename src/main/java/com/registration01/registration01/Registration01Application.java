157783377e1814143db990c052e3bb60b18e096b
javadev797982 <javadev797982@gmail.com>
2024-02-14 12:34:36 +0530

    111.1.1.1 create Registration entity

---
+++ src/main/java/com/registration01/registration01/Registration01Application.java
@@ -0,0 +1,13 @@
+package com.registration01.registration01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Registration01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Registration01Application.class, args);
+	}
+
+}
